a2c3f556becb57e6ee9250496adeb492ded68cc6
amandamo123 <moman2022@163.com>
2022-09-04 05:33:32 +0100

    feat: use keyword to search products

--- src/main/java/com/mman/controller/ProductController.java
+++ src/main/java/com/mman/controller/ProductController.java
@@ -1,6 +1,8 @@
 package com.mman.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.mman.entity.Product;
 import com.mman.entity.User;
 import com.mman.exception.MallException;
 import com.mman.result.ResponseEnum;
@@ -10,6 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.stereotype.Controller;
@@ -17,6 +20,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * <p>
@@ -57,6 +61,33 @@ public class ProductController {
             // 登录用户
             // 查询该用户的购物车记录
         }
+        // 商品分类
+        modelAndView.addObject("list", this.productCategoryService.buildProductCategoryMenu());
+        return modelAndView;
+    }
+
+    @PostMapping("/search")
+    public ModelAndView search(String keyWord, HttpSession session) {
+        if (keyWord == null) {
+            log.info("【商品列表】参数为空");
+            throw new MallException(ResponseEnum.PARAMETER_NULL);
+        }
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("productList");
+        // 模糊查询数据
+        QueryWrapper<Product> queryWrapper = new QueryWrapper<>();
+        queryWrapper.like("name", keyWord);
+        modelAndView.addObject("productList", this.productService.list(queryWrapper));
+        // 判断是否为登录用户
+        User user = (User) session.getAttribute("user");
+        if (user == null) {
+            //未登录
+            modelAndView.addObject("cartList", new ArrayList<>());
+        } else {
+            // 登录用户
+            // 查询该用户的购物车记录
+        }
+        // 商品分类
         modelAndView.addObject("list", this.productCategoryService.buildProductCategoryMenu());
         return modelAndView;
     }
